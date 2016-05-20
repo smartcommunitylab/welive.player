a0d78164d4f36ab91b36e98662c0bdc70959b79e
Nawaz Khurshid <nawaz1981@gmail.com>
2016-05-20 15:44:18 +0200

    Added 'terms of service' in italian.

--- weliveplayer-mobile/www/js/services/ConfigSrv.js
+++ weliveplayer-mobile/www/js/services/ConfigSrv.js
@@ -23,7 +23,7 @@ angular.module('weliveplayer.services.config', [])
         }
 
         var CREDITINFOP1 = {
-            "it": "The WeLive project has been financed under European Commission's H2020 programme for research, development and innovation under agreement #64584",
+            "it": "Il progetto WeLive è stato finanziato dal programma H2020 della Commissione Europea per la ricerca, lo sviluppo tecnologico e l’ innovazione secondo l’accordo N° 645845",
             "en": "The WeLive project has been financed under European Commission's H2020 programme for research, development and innovation under agreement #64584",
             "fi": "The WeLive project has been financed under European Commission's H2020 programme for research, development and innovation under agreement #64584",
             "es": "The WeLive project has been financed under European Commission's H2020 programme for research, development and innovation under agreement #64584",
