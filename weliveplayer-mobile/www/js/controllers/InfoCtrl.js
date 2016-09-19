e08598eea7fa3abe93f9e5438f8559a03226d998
Nawaz Khurshid <nawaz1981@gmail.com>
2016-09-19 14:23:57 +0200

    Fix for updated version codes.

--- weliveplayer-mobile/www/js/controllers/InfoCtrl.js
+++ weliveplayer-mobile/www/js/controllers/InfoCtrl.js
@@ -4,7 +4,7 @@ angular.module('weliveplayer.controllers.info', [])
         cordova.getAppVersion(function(version) {
             $scope.version = $filter('translate')('lbl_version') + " " + version;
         }, function(error) {
-            $scope.version = $filter('translate')('lbl_version') + "0.2.5";
+            $scope.version = $filter('translate')('lbl_version') + "0.2.8";
             }
         );
 
